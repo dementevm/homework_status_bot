ec137ecad5dee2908442503c9b27c2c73d489873
Mikhail Dementev <mikhaildementev@MacBook-Pro-Mikhail.local>
2025-03-10 19:41:09 +0300

    Доработка статусов

--- main.py
+++ main.py
@@ -15,19 +15,35 @@ URL = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 bot = TelegramBot(TELEGRAM_TOKEN)
+last_status = ''
 
 
 def parse_homework_status(homework):
+    global last_status
+    statuses = (
+        'reviewing',
+        'rejected',
+    )
     homework_name = homework['homework_name']
-    if homework.get('status') is None:
+    homework_status = homework.get('status')
+    if homework_status is None:
         time.sleep(300)
         return main()
-    if homework.get('status') == 'rejected':
+    if homework_status == statuses[0]:
+        if last_status == statuses[0]:
+            time.sleep(300)
+            return main()
+        else:
+            last_status = statuses[0]
+            return 'Ревьюер начала проверять работу'
+    elif homework_status == statuses[1]:
         verdict = 'К сожалению в работе нашлись ошибки.'
-
+        last_status = ''
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему ' \
-                  'уроку.'
+        verdict = (
+            'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+        )
+        last_status = ''
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
@@ -39,7 +55,7 @@ def get_homework_statuses(current_timestamp):
         homework_statuses = requests.get(URL, params=params, headers=HEADERS)
         return homework_statuses.json()
     except requests.exceptions as e:
-        print(f'Проблемы с requests. Ошибка: {e}')
+        bot.send_message(f'Проблемы с requests. Ошибка: {e}', CHAT_ID)
         time.sleep(300)
         main()
 
